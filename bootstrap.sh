d2c622223a098c9e17554a98762c6edd68aadcd3
Ali Akrour <ali.akrour@gmail.com>
2020-07-16 18:12:34 +0200

    Légère optimisation de l'installation des outils

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
+echo "*************************************"
+echo "* [1]: AJOUT DES DEPOTS NECESSAIRES *"
+echo "*************************************"
+apt-add-repository --yes --update ppa:ansible/ansible
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-add-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
 echo "********************************************************"
-echo "* [1]: MISE A JOUR DE LA LISTE DES PAQUETS DISPONIBLES *"
+echo "* [2]: MISE A JOUR DE LA LISTE DES PAQUETS DISPONIBLES *"
 echo "********************************************************"
 apt-get update
 
 echo "****************************"
-echo "* [2]: INSTALLATION DE VIM *"
+echo "* [3]: INSTALLATION DE VIM *"
 echo "****************************"
 apt-get install -y vim
 
 echo "********************************"
-echo "* [3]: INSTALLATION DE ANSIBLE *"
+echo "* [4]: INSTALLATION DE ANSIBLE *"
 echo "********************************"
-apt-get install -y software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt-get install -y ansible
+apt-get install -y software-properties-common ansible
 
 echo "*******************************"
-echo "* [4]: INSTALLATION DE DOCKER *"
+echo "* [5]: INSTALLATION DE DOCKER *"
 echo "*******************************"
-apt-get install -y apt-transport-https ca-certificates curl gnupg-agent
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent docker-ce docker-ce-cli containerd.io
 usermod -aG docker vagrant
 
